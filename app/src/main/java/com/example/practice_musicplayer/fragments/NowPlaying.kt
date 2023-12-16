b00344ca866952a8a31bd0d71cf1e3c76b936c45
devfast01 <iot123tech@gmail.com>
2023-12-16 20:55:23 +0500

    add now playing  fragment

---
+++ app/src/main/java/com/example/practice_musicplayer/fragments/NowPlaying.kt
@@ -0,0 +1,26 @@
+package com.example.practice_musicplayer.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.practice_musicplayer.R
+
+class NowPlaying : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_now_playing, container, false)
+    }
+
+}
