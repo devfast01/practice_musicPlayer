988f0f0c3850225f096d7927584c34969d0b1a2b
devfast01 <iot123tech@gmail.com>
2023-12-22 16:22:55 +0500

    MusicInterface

--- app/src/main/java/com/example/practice_musicplayer/activities/MusicInterface.kt
+++ app/src/main/java/com/example/practice_musicplayer/activities/MusicInterface.kt
@@ -3,10 +3,17 @@ package com.example.practice_musicplayer.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.practice_musicplayer.R
+import com.example.practice_musicplayer.databinding.ActivityMusicInterfaceBinding
 
 class MusicInterface : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMusicInterfaceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_music_interface)
+        binding = ActivityMusicInterfaceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
