a0efd511831ff130cb15b38b0db70f651fdc4732
devfast01 <iot123tech@gmail.com>
2024-01-08 20:34:17 +0500

    customize  viewPager

--- app/src/main/java/com/example/practice_musicplayer/ViewPager.kt
+++ app/src/main/java/com/example/practice_musicplayer/ViewPager.kt
@@ -2,10 +2,26 @@ package com.example.practice_musicplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.practice_musicplayer.adapters.AdapterCarousel
+import com.example.practice_musicplayer.databinding.ActivityViewPagerBinding
+import com.example.practice_musicplayer.utils.DpData
+import com.example.practice_musicplayer.utils.ViewPagerExtensions.addCarouselEffect
 
 class ViewPager : AppCompatActivity() {
+    private val binding by lazy { ActivityViewPagerBinding.inflate(layoutInflater) }
+    private val adapter by lazy { AdapterCarousel() }
+    private val dpData by lazy { DpData() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_pager)
+        setContentView(binding.root)
+
+        initViewPager()
+    }
+
+    private fun initViewPager() {
+        binding.viewPager.addCarouselEffect()
+        binding.viewPager.adapter = adapter
+        adapter.submitList(dpData.dummyData)
     }
 }
---
+++ app/src/main/java/com/example/practice_musicplayer/utils/DpData.kt
@@ -0,0 +1,12 @@
+package com.example.practice_musicplayer.utils
+
+class DpData {
+
+    val dummyData = listOf(
+        "Hello Developers",
+        "Hope, you are all fine!",
+        "Sohaib here!",
+        "Enjoy this new feature.",
+        "I made it pretty easier for you by adding an extension function."
+    )
+}
